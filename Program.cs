84709190f4a5e991ca9410f472ca3f2bd39f12f3
Antonis <lolplayer1231@outlook.com>
2026-02-21 16:23:11 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CRUDPurchaseCRUDVendor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
