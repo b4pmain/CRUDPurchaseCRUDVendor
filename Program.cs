5be9bff6b88e29df8b83885347d9773f5109f72f
Antonis <lolplayer1231@outlook.com>
2026-02-21 16:52:13 +0800

    Update 1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,64 @@
-﻿namespace CRUDPurchaseCRUDVendor
+﻿using System;
+
+namespace CRUDPurchaseCRUDVendor
 {
     internal class Program
     {
+        static List<string> purchase = new List<string>();
+        static List<string> vendor = new List<string>();
+        static bool OnSession = true;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Vendors();
+            while (OnSession)
+            {
+                Menu();
+            }
+            
         }
+        static void Menu()
+        {
+            string userChoice;
+            Console.WriteLine("Welcome to Vendor + Purchase Management\nAdd | Search | Update | Delete | Exit");
+            userChoice = Console.ReadLine();
+            switch (userChoice)
+            {
+                case "Add":
+                    break;
+                case "Search":
+                    break;
+                case "Update":
+                    break;
+                case "Delete":
+                    break;
+                case "Exit":
+                    OnSession = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid Choice.");
+                    break;
+            }
+        }
+
+        static void Purchase()
+        {
+
+        }
+        static void AddVendor(string vendorName)
+        {
+            Console.WriteLine($"Added {vendorName}.");
+            vendor.Add(vendorName);
+        }
+
+        static void Vendors()
+        {
+            vendor.Add("Nescafe");
+            vendor.Add("San Miguel Corporation");
+            vendor.Add("Rebisco");
+            vendor.Add("Nestle");
+            vendor.Add("Oishi");
+        }
+
     }
 }
